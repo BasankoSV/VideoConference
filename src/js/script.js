a2f7426eebe3af06305314bc20af4e7d12f1ea34
BasankoSV <basankosv@gmail.com>
2021-09-07 19:58:15 +0500

    Add "Pricing plan", add interactive JS "switch/cost"

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,32 @@
-const planSwitcher = document.querySelector('.plan-switcher')
+const PRICE = {
+  annually: {
+    basic: ['No', ''],
+    pro: ['$499', '/year'],
+    business: ['$999', '/year']
+  },
+  monthly: {
+    basic: ['Free', ''],
+    pro: ['$49', '/mo'],
+    business: ['$99', '/mo']
+  }
+}
 
-planSwitcher.addEventListener('click', e => {
-  e.preventDefault()
-  if (e.target.tagName === "BUTTON") {
+const planSwitcher = document.querySelector('.plan-switcher')
+const prices = document.querySelectorAll('.price')
+
+planSwitcher.addEventListener('click', event => {
+  event.preventDefault()
+  let target = event.target
+  if (target.tagName === "BUTTON") {
     const buttons = planSwitcher.children
     for (let i = 0; i < buttons.length; i++) {
       buttons[i].classList.remove('switch-on')
     }
-    e.target.classList.add('switch-on')
+    target.classList.add('switch-on')
+    let period = target.textContent.toLowerCase()
+    for (const price of prices) {
+      let type = price.dataset.priceType
+      price.innerHTML = `${PRICE[period][type][0]}<span>${PRICE[period][type][1]}</span>`
+    }
   }
 })
