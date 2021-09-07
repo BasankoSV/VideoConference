1ab9105a48744d2d530eafca0e25e615ca0514f7
BasankoSV <basankosv@gmail.com>
2021-09-07 09:18:01 +0500

    Add JS for switch "Annually/Monthly"

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
-console.log('Hello world!')
+const planSwitcher = document.querySelector('.plan-switcher')
+
+planSwitcher.addEventListener('click', e => {
+  e.preventDefault()
+  if (e.target.tagName === "BUTTON") {
+    const buttons = planSwitcher.children
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].classList.remove('switch-on')
+    }
+    e.target.classList.add('switch-on')
+  }
+})
